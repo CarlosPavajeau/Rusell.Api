835aece6dfd6311f606d3e456e031222caad68e6
cantte <cantte098@gmail.com>
2021-12-09 18:25:03 -0500

    feat: convert string to database format

--- src/Shared/Extensions/StringExtensions.cs
+++ src/Shared/Extensions/StringExtensions.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text;
 
 namespace Rusell.Shared.Extensions;
@@ -22,5 +23,13 @@ public static class StringExtensions
     public static string FromBase64(this string str)
     {
         return Encoding.UTF8.GetString(Convert.FromBase64String(str));
+    }
+
+    private static readonly Func<char, string> AddUnderscoreBeforeCapitalLetter =
+        x => char.IsUpper(x) ? "_" + x : x.ToString(CultureInfo.InvariantCulture);
+
+    public static string ToDatabaseFormat(this string value)
+    {
+        return string.Concat(value.Select(AddUnderscoreBeforeCapitalLetter))[1..].ToLowerInvariant();
     }
 }
