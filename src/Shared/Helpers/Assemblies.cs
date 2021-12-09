c513b033834946008d90a6ff856479478fd1c72b
cantte <cantte098@gmail.com>
2021-12-09 18:24:31 -0500

    refactor(shared): rename Addresses assembly name

--- src/Shared/Helpers/Assemblies.cs
+++ src/Shared/Helpers/Assemblies.cs
@@ -2,5 +2,5 @@ namespace Rusell.Shared.Helpers;
 
 public static class Assemblies
 {
-    public static string Addresses => "Addresses";
+    public static string Addresses => "Rusell.Addresses";
 }
