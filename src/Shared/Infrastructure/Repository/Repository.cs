985f147a81d0b56bde2c6c0debb45e029ed53e4d
cantte <cantte098@gmail.com>
2021-12-10 10:10:30 -0500

    feat: base repository implementation added

---
+++ src/Shared/Infrastructure/Repository/Repository.cs
@@ -0,0 +1,44 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Rusell.Shared.Domain.Repository;
+
+namespace Rusell.Shared.Infrastructure.Repository;
+
+public abstract class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class
+{
+    protected readonly DbContext Context;
+
+    protected Repository(DbContext context)
+    {
+        Context = context;
+    }
+
+    public async Task Save(TEntity entity)
+    {
+        await Context.Set<TEntity>().AddAsync(entity);
+        await Context.SaveChangesAsync();
+    }
+
+    public abstract Task<TEntity> Find(TKey key, bool noTracking);
+
+    public async Task<TEntity> Find(TKey key)
+    {
+        return await Find(key, true);
+    }
+
+    public async Task<bool> Any(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await Context.Set<TEntity>().AnyAsync(predicate);
+    }
+
+    public async Task<IEnumerable<TEntity>> SearchAll()
+    {
+        return await Context.Set<TEntity>().AsNoTracking().ToListAsync();
+    }
+
+    public async Task Delete(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+        await Context.SaveChangesAsync();
+    }
+}
