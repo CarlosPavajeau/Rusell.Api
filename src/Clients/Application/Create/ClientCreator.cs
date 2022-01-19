ad14d2a5a77ec44b1952c46d1e668700a0c7ba89
cantte <cantte098@gmail.com>
2022-01-18 20:42:41 -0500

    feat(clients): publish client domamin envents

--- src/Clients/Application/Create/ClientCreator.cs
+++ src/Clients/Application/Create/ClientCreator.cs
@@ -1,20 +1,25 @@
 using Mapster;
 using Rusell.Clients.Domain;
+using Rusell.Shared.Domain.Bus.Event;
 
 namespace Rusell.Clients.Application.Create;
 
 public class ClientCreator
 {
     private readonly IClientsRepository _repository;
+    private readonly IEventBus _eventBus;
 
-    public ClientCreator(IClientsRepository repository)
+    public ClientCreator(IClientsRepository repository, IEventBus eventBus)
     {
         _repository = repository;
+        _eventBus = eventBus;
     }
 
     public async Task Create(CreateClientCommand command)
     {
         var client = command.Adapt<Client>();
+
         await _repository.Save(client);
+        await _eventBus.Publish(client.PullDomainEvents());
     }
 }
