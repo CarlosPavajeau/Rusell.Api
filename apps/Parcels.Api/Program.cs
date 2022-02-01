695134828d5c17c692791ad1082beb40157d01be
cantte <cantte098@gmail.com>
2022-02-01 11:07:27 -0500

    feat(parcels): migrate database on app startup

--- apps/Parcels.Api/Program.cs
+++ apps/Parcels.Api/Program.cs
@@ -1,4 +1,6 @@
 using Rusell.Parcels.Api.Extensions.DependencyInjection;
+using Rusell.Parcels.Shared.Infrastructure.Persistence.EntityFramework;
+using Rusell.Shared.Extensions.DependencyInjection;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsProduction()) app.MigrateDatabase<ParcelsDbContext>();
 
 if (app.Environment.IsDevelopment())
 {
